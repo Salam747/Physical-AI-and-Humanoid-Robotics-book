9bb3736c44f9325cb3d26d52646ee603d588e545
Abdul Salam <salamabsul466@gmail.com>
2025-12-05 20:37:42 +0500

    chore: Finalize Docusaurus configuration and cleanup

--- Robotic book/docusaurus.config.ts
+++ Robotic book/docusaurus.config.ts
@@ -22,8 +22,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'yourusername', // Usually your GitHub org/user name.
+  projectName: 'physical-ai-humanoid-book', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/yourusername/physical-ai-humanoid-book/tree/main/',
         },
         blog: false,
         theme: {
